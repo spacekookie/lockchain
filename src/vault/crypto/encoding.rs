fcc82750219e5c230c2297afb4aa46035fe6c6db
Katharina Fey <kookie@spacekookie.de>
2017-12-13 00:20:47 +0100

    Adding some better utility functions to work with base64 strings

--- src/vault/crypto/encoding.rs
+++ src/vault/crypto/encoding.rs
@@ -2,7 +2,22 @@
 //! 
 
 use std::fmt::Write;
+use std;
 use base64;
+
+
+/// Takes a utf-8 string of raw binary data and converts itto base64 encoded form
+pub fn encode_base64(data: &str) -> String {
+    return base64::encode(data.as_bytes());
+}
+
+/// Takes a base64 string and converts it to raw binary data
+pub fn decode_base64(base64: &str) -> String {
+    let vec = base64::decode(base64).unwrap();
+    let decoded = unsafe { std::str::from_utf8_unchecked(&vec) };
+    return String::from(decoded);
+}
+
 
 /// Encode a byte string as base64 symbols
 pub fn base64(data: &Vec<u8>) -> String {
