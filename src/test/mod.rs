c30007af8d89a04164d9418727e4bb794b0dd3d4
Katharina Fey <kookie@spacekookie.de>
2017-12-15 11:50:07 +0100

    Adding testing module – trying to find out where the bug is!

---
+++ src/test/mod.rs
@@ -0,0 +1,12 @@
+//! Unit testing module for lockchain
+//! 
+//! Purpose of this module is to properly test all the functions involved 
+//! in lockchain, since even a small bug can completely break crypto
+//! 
+#![cfg(test)]
+
+
+#[test]
+fn init() {
+
+}
