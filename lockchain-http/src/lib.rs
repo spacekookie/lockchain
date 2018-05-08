cd0917de6beb83bd0d84af1cfbb6e5e939814e92
Katharina Fey <kookie@spacekookie.de>
2018-05-08 12:58:09 +0200

    More crate metadata for crates io

--- lockchain-http/src/lib.rs
+++ lockchain-http/src/lib.rs
@@ -1,4 +1,13 @@
 //! A plug and play http interface layer for various lockchain components
 
 extern crate gotham_serde_json_body_parser as goth_json;
+extern crate gotham;
 
+/// Contains API internal state and metadata
+pub struct Server {
+
+}
+
+pub enum ApiFeature {
+    BasicIo
+}
